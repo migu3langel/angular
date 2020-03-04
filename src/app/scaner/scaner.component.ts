a9dc331b547df1db84d25e26a9ebe65fd9698ce8
miguelangel <migu3lvareta@hotmail.com>
2020-03-04 09:20:34 -0600

    Calendario Español

--- src/app/scaner/scaner.component.ts
+++ src/app/scaner/scaner.component.ts
@@ -23,6 +23,7 @@ datasource: any;
 selectedCar1: Scaner;
 value: Date;
 value2: Date;
+es: any;
 
 fecha: string;
 
@@ -65,6 +66,16 @@ setFecha(){
     ) { }
 
   ngOnInit() {
+    this.es = {
+      firstDayOfWeek: 1,
+      dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+      dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+      dayNamesMin: [ "D","L","M","X","J","V","S" ],
+      monthNames: [ "enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre" ],
+      monthNamesShort: [ "ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic" ],
+      today: 'Hoy',
+      clear: 'Borrar'
+  }
 
 
     this.scanService.getScan().subscribe(scan => {
